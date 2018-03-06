ca55341ad8e9b5fde4da7655a6ea800aaf053e1d
violoncelloch <22591354+violoncelloch@users.noreply.github.com>
2018-03-06 16:46:04 +0100

    removed uk-height-viewport

--- views/build/hero-position.php
+++ views/build/hero-position.php
@@ -1,4 +1,4 @@
-<div class="uk-height-viewport <?= $params['blend'] ? $params['color']:'' ?> uk-background-image uk-background-cover <?= ($params['light'] ? 'uk-light':'')  ?>" style="background-image: url(<?= $params['hero_image'] ?>)">
+<div class="<?= $params['blend'] ? $params['color']:'' ?> uk-background-image uk-background-cover <?= ($params['light'] ? 'uk-light':'')  ?>" style="background-image: url(<?= $params['hero_image'] ?>)">
   <?php foreach ($widgets as $widget): ?>
     <?= $widget->get('result') ?>
   <?php endforeach; ?>
